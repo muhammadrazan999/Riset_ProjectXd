366bb46fe98f6a6b8a44a33c4bedc3f106470cd7
muhammadrazan999 <61842485+muhammadrazan999@users.noreply.github.com>
2020-10-22 05:14:37 +0700

    Donat.cpp
    grafik donat

---
+++ Donat.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+
+using namespace std;
+
+
+int main(){
+	
+    float A = 0, B = 0;
+    float i = 0, j = 0;
+    int k = 0;
+    float z[1760];
+    char b[1760];
+    printf("\x1b[2J");
+    for(;;) {
+        memset(b,32,1760);
+        memset(z,0,7040);
+        for(j=0; j <= 6.28; j += 0.07) {
+            for(i=0; i <= 6.28; i += 0.02) {
+                float c = sin(i*1);
+                float d = cos(j*1);
+                float e = sin(A*1);
+                float f = sin(j*1);
+                float g = cos(A*1);
+                float h = d + 2;
+                float D = 1 / (c * h * e + f * g + 5);
+                float l = cos(i);
+                float m = cos(B);
+                float n = sin(B);
+                float t = c * h * g - f * e;
+                int x = 40 + 30 * D * (l * h * m - t * n);
+                int y= 12 + 15 * D * (l * h * n + t * m);
+                int o = x + 80 * y;
+                int N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
+                if(22 > y && y > 0 && x > 0 && 80 > x && D > z[o]) {
+                    z[o] = D;
+                    b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
+                }
+            }
+        }
+        printf("\x1b[H");
+        for(k = 0; k <= 1761; k++) {
+            putchar(k % 80 ? b[k] : 10);
+            A += 0.00004;
+            B += 0.00002;
+        }
+        usleep(30000);
+    }
+    return 0;
+}
